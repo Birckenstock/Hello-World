ecb59588d4822b0c1b98de74e8b920818c91fecf
Birckenstock <54004432+Birckenstock@users.noreply.github.com>
2019-08-11 20:35:05 +0200

    Add files via upload

---
+++ spring-boot-helloworld3/src/main/java/com/example/springboothelloworld3/SpringBootHelloworld3Application.java
@@ -0,0 +1,13 @@
+package com.example.springboothelloworld3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloworld3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloworld3Application.class, args);
+	}
+
+}
